3e0c97c46017e46c4cdb98f61a71fe2d498a0d1a
Kono Tyran <Kono@koifysh.dev>
2024-07-25 21:40:28 -0700

    Initial commit

---
+++ src/client/kotlin/dev/koifysh/ArchipelagoRandomizerClient.kt
@@ -0,0 +1,9 @@
+package dev.koifysh
+
+import net.fabricmc.api.ClientModInitializer
+
+object ArchipelagoRandomizerClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
